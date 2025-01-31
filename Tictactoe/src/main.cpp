ea652de657919a0e536275e7c92d2561949c9646
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-01 00:00:09 +0100

    d

--- Tictactoe/src/main.cpp
+++ Tictactoe/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_audio.h>
+#include < SDL3 / SDL_video.h >
 
 
 int main()
@@ -9,15 +10,18 @@ int main()
 	SDL_Renderer* renderer = nullptr;
 
 	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_EVENTS);
 
-	SDL_CreateWindowAndRenderer("Tictactoe", 320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer);
-	//SDL_RenderSetScale(renderer, 4, 4);
+	SDL_CreateWindowAndRenderer("Tictactoe", 640, 480, SDL_WINDOW_MOUSE_CAPTURE, &window, &renderer);
+	//SDL_Surface(SDL_LoadBMP());
+	//SDL_SetWindowIcon();               (SDL_LoadBMP(“”), NULL);
+	SDL_SetRenderScale(renderer, 4, 4);
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 100, 100, 100, 0);
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderPoint(renderer, 640.0 / 2, 480.0 / 2);
+	//SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	//SDL_RenderPoint(renderer, 640.0 / 2, 480.0 / 2);
 	SDL_RenderPresent(renderer);
 	SDL_Delay(20000);
 
