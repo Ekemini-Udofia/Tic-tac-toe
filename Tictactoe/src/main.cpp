b5a85cf5dba45fd8b0f792b32b53c163ffdf1f21
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-01 16:06:20 +0100

    Fixed the SDL_mapRGB Bugr by creating a custom uint32 color. Was succesfully able to render the background

--- Tictactoe/src/main.cpp
+++ Tictactoe/src/main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 
-int main()
-{
+int main(int argc, char** args){
+	SDL_Window* window = NULL;
+	SDL_Surface* winSurface = NULL;
+
 	const char* title = "Tictactoe";
-	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-	SDL_Window* window = SDL_CreateWindow(title, 1280, 720, SDL_WINDOW_ALWAYS_ON_TOP);
+
+	Uint8 r = 255;  
+	Uint8 g = 90;   
+	Uint8 b = 120;  
+	Uint8 a = 255;  
+
+	Uint32 color = (r << 24) | (g << 16) | (b << 8) | a;
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+	{
+		std::cout << "Failed to initialize SDL" << SDL_GetError() << '\n';
+		system("PAUSE");
+		return 1;
+	}
+	window = SDL_CreateWindow(title, 1280, 720, SDL_WINDOW_ALWAYS_ON_TOP);
 	if (!window) {
 		std::cout << "Failed to create a window! Error: " << SDL_GetError << '\n';
+		system("PAUSE");
+		return 1;
 	}
-	SDL_Surface* winSurface = SDL_GetWindowSurface(window);
+	winSurface = SDL_GetWindowSurface(window);
+	if (!winSurface)
+	{
+		std::cout << "Failed to initialize the Windows Surface" << SDL_GetError() << '\n';
+		system("PAUSE");
+		return 1;
+	}
 
 	// do drawing
-	//Uint32 color = SDL_MapRGB(*SDL_PixelFormatDetails::format, NULL, 255, 90, 120);
-	//SDL_FillSurfaceRect(winSurface, NULL, color);
+	SDL_FillSurfaceRect(winSurface, NULL, color);
 
 	SDL_UpdateWindowSurface(window);
-	SDL_Delay(10000);
+	system("PAUSE");
 	SDL_DestroyWindow(window);
-	window = NULL;
-	winSurface = NULL;
 	SDL_Quit();
 }
