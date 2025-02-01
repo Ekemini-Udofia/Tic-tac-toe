b4a38514e2d2349dcdefca415ad123c4f8bd82e7
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-01 15:44:49 +0100

    Removed redundant header files

--- Tictactoe/src/main.cpp
+++ Tictactoe/src/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_audio.h>
-#include <SDL3/SDL_video.h>
-
-#include <SDL3/SDL_init.h>
 
 int main()
 {
@@ -16,8 +12,8 @@ int main()
 	SDL_Surface* winSurface = SDL_GetWindowSurface(window);
 
 	// do drawing
-	Uint32 color = SDL_MapRGB(*SDL_PixelFormatDetails::format, NULL, 255, 90, 120);
-	SDL_FillSurfaceRect(winSurface, NULL, color);
+	//Uint32 color = SDL_MapRGB(*SDL_PixelFormatDetails::format, NULL, 255, 90, 120);
+	//SDL_FillSurfaceRect(winSurface, NULL, color);
 
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(10000);
