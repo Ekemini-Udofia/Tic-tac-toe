ddf455d66a6a42b487b78f9fd1553398e4a494cc
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-02 15:19:56 +0100

    Added the main game loop. Fiexd some issues. Program still hangs though

--- Tictactoe/src/main.cpp
+++ Tictactoe/src/main.cpp
@@ -6,6 +6,10 @@
 int main(int argc, char** args){
 	SDL_Window* window = NULL;
 	SDL_Surface* winSurface = NULL;
+	SDL_Renderer* renderer = nullptr;
+
+
+
 	const char* title = "Tictactoe";
 
 	Uint8 r = 255;  
@@ -21,7 +25,7 @@ int main(int argc, char** args){
 		system("PAUSE");
 		return 1;
 	}
-	window = SDL_CreateWindow(title, 1280, 720, SDL_WINDOW_ALWAYS_ON_TOP);
+	window = SDL_CreateWindow(title, 1280, 720, SDL_WINDOW_RESIZABLE);
 	if (!window) {
 		std::cout << "Failed to create a window! Error: " << SDL_GetError << '\n';
 		system("PAUSE");
@@ -35,11 +39,32 @@ int main(int argc, char** args){
 		return 1;
 	}
 
-	// do drawing
-	SDL_FillSurfaceRect(winSurface, NULL, color);
-	SDL_UpdateWindowSurface(window);
-	system("PAUSE");
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+
+	SDL_Event main;
+	bool running = true;
+	while (running)
+	{
+		SDL_PollEvent(nullptr);
+
+		while (SDL_PollEvent(&main))
+		{
+			if (main.type == SDL_EVENT_QUIT)
+			{
+				running = false;
+			}
+		}
+		SDL_FRect rectangle{ 10, 10, 20, 250 };
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &rectangle);
+
+		SDL_RenderPresent(renderer);
+
+		SDL_FillSurfaceRect(winSurface, NULL, color);
+		SDL_UpdateWindowSurface(window);
+		system("PAUSE");
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
 	return 0;	
 }
