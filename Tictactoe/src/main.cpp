1c5f1932e3a199400bb6276f9d9eae677f9f7c9e
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-03 21:12:03 +0100

    Fixed all the bugs, created all the game initialization functions, added the first bitmap, created the loading fumction. Fixed the bug causing the window to crash randomly. Everything works fine at this stage (Thanks to the lazy foo tutorials

--- Tictactoe/src/main.cpp
+++ Tictactoe/src/main.cpp
@@ -6,70 +6,115 @@
 constexpr int screen_width = 1000;
 constexpr int screen_height = 700;
 
+SDL_Window* window{ nullptr };
+SDL_Surface* winSurface{ nullptr };
+SDL_Renderer* renderer{ nullptr };
+SDL_Surface* Test_surface{ nullptr };
+
+
+
+const char* title = "Tictactoe";
+
+Uint8 r = 255;
+Uint8 g = 90;
+Uint8 b = 120;
+Uint8 a = 255;
+
+Uint32 color = (r << 24) | (g << 16) | (b << 8) | a;
 
 
 
 
-int main(int argc, char** args) {
-	SDL_Window* window{ nullptr };
-	SDL_Surface* winSurface{ nullptr };
-	SDL_Renderer* renderer{ nullptr };
-
-
-
-	const char* title = "Tictactoe";
-
-	Uint8 r = 255;  
-	Uint8 g = 90;   
-	Uint8 b = 120;  
-	Uint8 a = 255;  
-
-	Uint32 color = (r << 24) | (g << 16) | (b << 8) | a;
-
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
+bool init()
+{
+	bool success{ true };
+	if (!SDL_Init(SDL_INIT_VIDEO) )
 	{
-		SDL_Log( "Failed to initialize SDL: %s\n", SDL_GetError() );
-		system("PAUSE");
-		return 1;
+		SDL_Log("Failed to initialize SDL. SDL Error %s\n", SDL_GetError());
+		success = false;
 	}
-	window = SDL_CreateWindow(title, screen_width, screen_height, SDL_WINDOW_RESIZABLE);
-	if (!window) {
-		SDL_Log( "Failed to create a window! Error: %s\n", SDL_GetError() );
-		system("PAUSE");
-		return 1;
-	}
-	winSurface = SDL_GetWindowSurface(window);
-	if (!winSurface)
+	else
 	{
-		SDL_Log( "Failed to initialize the Windows Surface: %s\n", SDL_GetError() );
-		system("PAUSE");
-		return 1;
-	}
-
-
-	SDL_Event main;
-	bool running = true;
-	while (running)
-	{
-		SDL_PollEvent(nullptr);
-
-		while (SDL_PollEvent(&main))
+		if (( window = SDL_CreateWindow(title, screen_width, screen_height, 0 )) == nullptr) 
 		{
-			if (main.type == SDL_EVENT_QUIT)
-			{
-				running = false;
-			}
+			SDL_Log("Failed to create a window! Error: %s\n", SDL_GetError());
+			success = false;
 		}
+		else
+		{
+			winSurface = SDL_GetWindowSurface(window);
 
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-		SDL_RenderPresent(renderer);
-
-		SDL_FillSurfaceRect(winSurface, NULL, color);
-		SDL_UpdateWindowSurface(window);
-		system("PAUSE");
-		SDL_DestroyWindow(window);
-		SDL_Quit();
+		}
 	}
-	return 0;	
+	return success;
+
 }
+
+bool load_media()
+{
+	bool success{ true };
+	std::string imagepath{ "App_Resources/app.bmp" };
+	if ((Test_surface = SDL_LoadBMP(imagepath.c_str())) == nullptr)
+	{
+		SDL_Log("Failed to load image. SDL Error: %s\n", imagepath.c_str(), SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void Destroy_Window()
+{
+	
+	SDL_DestroySurface(winSurface);
+	winSurface = nullptr;
+	SDL_DestroySurface(Test_surface);
+	Test_surface = nullptr;
+	SDL_DestroyWindow(window);
+	window = nullptr;
+	SDL_Quit();
+
+}
+
+int main(int argc, char* argv[])
+{
+	int exitcode{ 0 };
+
+	if (!init())
+	{
+		SDL_Log("Failed to initialize SDL. SDL Error %s\n", SDL_GetError());
+		exitcode = 1;
+	}
+	else
+	{
+		if (!load_media())
+		{
+			SDL_Log("Failed to load media. SDL Error %s\n");
+			exitcode = 2;
+		}
+		else
+		{
+			bool quit{ false };
+			SDL_Event e;
+			SDL_zero(e);
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					if (e.type == SDL_EVENT_QUIT)
+					{
+						quit = true;
+					}
+				}
+				SDL_FillSurfaceRect(winSurface, nullptr, color);
+				SDL_BlitSurface(Test_surface, nullptr, winSurface, nullptr);
+				SDL_UpdateWindowSurface(window);
+			}
+
+		}
+	}
+
+	Destroy_Window();
+	return exitcode;
+}
+
